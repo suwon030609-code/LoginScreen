7f8542117cac2c78f654014f18856b8bf6ca145f
강준서 <suwon030609@gmail.com>
2026-04-02 17:25:44 +0900

    과제2 코딩과 테스트 완료
    Enter 키를 이용한 입력창 이동 및 로그인 실행 기능 구현
    MessageBox 대신 Label로 결과 메시지 출력하도록 변경
    Visible 속성을 이용한 메시지 표시/숨김 처리 구현
    기능 테스트 완료

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,7 @@
             txtID = new TextBox();
             txtPW = new TextBox();
             btnLogin = new Button();
+            lblErrorMsg = new Label();
             SuspendLayout();
             // 
             // lblAppName
@@ -54,6 +55,7 @@
             txtID.TabIndex = 2;
             txtID.Text = "아이디";
             txtID.Enter += txtID_Enter;
+            txtID.KeyDown += txtID_KeyDown;
             txtID.Leave += txtID_Leave;
             // 
             // txtPW
@@ -66,6 +68,7 @@
             txtPW.TabIndex = 3;
             txtPW.Text = "Password";
             txtPW.Enter += txtPW_Enter;
+            txtPW.KeyDown += txtPW_KeyDown;
             txtPW.Leave += txtPW_Leave;
             // 
             // btnLogin
@@ -73,7 +76,7 @@
             btnLogin.BackColor = Color.Khaki;
             btnLogin.Font = new Font("맑은 고딕", 24F, FontStyle.Regular, GraphicsUnit.Point, 129);
             btnLogin.ForeColor = Color.Olive;
-            btnLogin.Location = new Point(350, 705);
+            btnLogin.Location = new Point(350, 737);
             btnLogin.Name = "btnLogin";
             btnLogin.Size = new Size(900, 85);
             btnLogin.TabIndex = 1;
@@ -81,11 +84,25 @@
             btnLogin.UseVisualStyleBackColor = false;
             btnLogin.Click += btnLogin_Click;
             // 
+            // lblErrorMsg
+            // 
+            lblErrorMsg.AutoSize = true;
+            lblErrorMsg.Font = new Font("맑은 고딕", 13.875F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lblErrorMsg.ForeColor = Color.Red;
+            lblErrorMsg.Location = new Point(446, 602);
+            lblErrorMsg.Name = "lblErrorMsg";
+            lblErrorMsg.Size = new Size(711, 50);
+            lblErrorMsg.TabIndex = 4;
+            lblErrorMsg.Text = "아이디 또는 패스워드가 잘못 되었습니다.";
+            lblErrorMsg.TextAlign = ContentAlignment.TopCenter;
+            lblErrorMsg.Visible = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1574, 929);
+            Controls.Add(lblErrorMsg);
             Controls.Add(btnLogin);
             Controls.Add(txtPW);
             Controls.Add(txtID);
@@ -102,5 +119,6 @@
         private TextBox txtID;
         private TextBox txtPW;
         private Button btnLogin;
+        private Label lblErrorMsg;
     }
 }
